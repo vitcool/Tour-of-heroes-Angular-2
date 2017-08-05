8344891dfaddc43a4c9a1954a538efe6500696aa
Vitalii Kulyk <vitcool@ukr.net>
2017-08-05 15:19:40 +0300

    TOG_0: the hero editor added (2)

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Hero } from "./hero";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "Tour of Heroes";
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
 }
